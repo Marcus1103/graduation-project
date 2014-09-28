8c49f46210d06fdd0c0181771aba62ea7394af4b
Marcus <598819790@qq.com>
2014-09-28 14:34:56 +0800

    pod update

---
+++ SmartHome/Pods/Pods-environment.h
@@ -0,0 +1,290 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFHTTPRequestOperationLogger
+#define COCOAPODS_POD_AVAILABLE_AFHTTPRequestOperationLogger
+#define COCOAPODS_VERSION_MAJOR_AFHTTPRequestOperationLogger 1
+#define COCOAPODS_VERSION_MINOR_AFHTTPRequestOperationLogger 0
+#define COCOAPODS_VERSION_PATCH_AFHTTPRequestOperationLogger 0
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 4
+
+// FoundationExtension/FoundationExtension
+#define COCOAPODS_POD_AVAILABLE_FoundationExtension_FoundationExtension
+#define COCOAPODS_VERSION_MAJOR_FoundationExtension_FoundationExtension 0
+#define COCOAPODS_VERSION_MINOR_FoundationExtension_FoundationExtension 55
+#define COCOAPODS_VERSION_PATCH_FoundationExtension_FoundationExtension 0
+
+// FoundationExtension/UIKitExtension
+#define COCOAPODS_POD_AVAILABLE_FoundationExtension_UIKitExtension
+#define COCOAPODS_VERSION_MAJOR_FoundationExtension_UIKitExtension 0
+#define COCOAPODS_VERSION_MINOR_FoundationExtension_UIKitExtension 55
+#define COCOAPODS_VERSION_PATCH_FoundationExtension_UIKitExtension 0
+
+// GIKPopoverBackgroundView/Core
+#define COCOAPODS_POD_AVAILABLE_GIKPopoverBackgroundView_Core
+#define COCOAPODS_VERSION_MAJOR_GIKPopoverBackgroundView_Core 0
+#define COCOAPODS_VERSION_MINOR_GIKPopoverBackgroundView_Core 0
+#define COCOAPODS_VERSION_PATCH_GIKPopoverBackgroundView_Core 1
+
+// GPUImage
+#define COCOAPODS_POD_AVAILABLE_GPUImage
+#define COCOAPODS_VERSION_MAJOR_GPUImage 0
+#define COCOAPODS_VERSION_MINOR_GPUImage 1
+#define COCOAPODS_VERSION_PATCH_GPUImage 4
+
+// JSONModel
+#define COCOAPODS_POD_AVAILABLE_JSONModel
+#define COCOAPODS_VERSION_MAJOR_JSONModel 0
+#define COCOAPODS_VERSION_MINOR_JSONModel 13
+#define COCOAPODS_VERSION_PATCH_JSONModel 0
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 6
+#define COCOAPODS_VERSION_PATCH_SDWebImage 0
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 6
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 0
+
+// SevenSwitch
+#define COCOAPODS_POD_AVAILABLE_SevenSwitch
+#define COCOAPODS_VERSION_MAJOR_SevenSwitch 1
+#define COCOAPODS_VERSION_MINOR_SevenSwitch 3
+#define COCOAPODS_VERSION_PATCH_SevenSwitch 0
+
+// UI7Kit
+#define COCOAPODS_POD_AVAILABLE_UI7Kit
+#define COCOAPODS_VERSION_MAJOR_UI7Kit 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit 18
+
+// UI7Kit/All
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_All
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_All 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_All 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_All 18
+
+// UI7Kit/Core
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_Core
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_Core 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_Core 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_Core 18
+
+// UI7Kit/UI7ActionSheet
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7ActionSheet
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7ActionSheet 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7ActionSheet 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7ActionSheet 18
+
+// UI7Kit/UI7ActivityViewController
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7ActivityViewController
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7ActivityViewController 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7ActivityViewController 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7ActivityViewController 18
+
+// UI7Kit/UI7AlertView
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7AlertView
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7AlertView 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7AlertView 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7AlertView 18
+
+// UI7Kit/UI7BarButtonItem
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7BarButtonItem
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7BarButtonItem 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7BarButtonItem 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7BarButtonItem 18
+
+// UI7Kit/UI7Button
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Button
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Button 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Button 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Button 18
+
+// UI7Kit/UI7Color
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Color
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Color 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Color 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Color 18
+
+// UI7Kit/UI7Font
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Font
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Font 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Font 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Font 18
+
+// UI7Kit/UI7NavigationBar
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7NavigationBar
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7NavigationBar 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7NavigationBar 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7NavigationBar 18
+
+// UI7Kit/UI7NavigationController
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7NavigationController
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7NavigationController 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7NavigationController 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7NavigationController 18
+
+// UI7Kit/UI7PickerView
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7PickerView
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7PickerView 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7PickerView 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7PickerView 18
+
+// UI7Kit/UI7PopoverController
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7PopoverController
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7PopoverController 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7PopoverController 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7PopoverController 18
+
+// UI7Kit/UI7ProgressView
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7ProgressView
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7ProgressView 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7ProgressView 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7ProgressView 18
+
+// UI7Kit/UI7SearchBar
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7SearchBar
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7SearchBar 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7SearchBar 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7SearchBar 18
+
+// UI7Kit/UI7SegmentedControl
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7SegmentedControl
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7SegmentedControl 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7SegmentedControl 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7SegmentedControl 18
+
+// UI7Kit/UI7Slider
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Slider
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Slider 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Slider 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Slider 18
+
+// UI7Kit/UI7Stepper
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Stepper
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Stepper 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Stepper 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Stepper 18
+
+// UI7Kit/UI7Switch/SevenSwitch
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Switch_SevenSwitch
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Switch_SevenSwitch 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Switch_SevenSwitch 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Switch_SevenSwitch 18
+
+// UI7Kit/UI7TabBar
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TabBar
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TabBar 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TabBar 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TabBar 18
+
+// UI7Kit/UI7TabBarController
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TabBarController
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TabBarController 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TabBarController 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TabBarController 18
+
+// UI7Kit/UI7TabBarItem
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TabBarItem
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TabBarItem 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TabBarItem 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TabBarItem 18
+
+// UI7Kit/UI7TableView
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TableView
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TableView 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TableView 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TableView 18
+
+// UI7Kit/UI7TableViewCell
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TableViewCell
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TableViewCell 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TableViewCell 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TableViewCell 18
+
+// UI7Kit/UI7TextField
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7TextField
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7TextField 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7TextField 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7TextField 18
+
+// UI7Kit/UI7Toolbar
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Toolbar
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Toolbar 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Toolbar 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Toolbar 18
+
+// UI7Kit/UI7View
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7View
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7View 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7View 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7View 18
+
+// UI7Kit/UI7ViewController
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7ViewController
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7ViewController 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7ViewController 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7ViewController 18
+
+// UI7Kit/UI7Window
+#define COCOAPODS_POD_AVAILABLE_UI7Kit_UI7Window
+#define COCOAPODS_VERSION_MAJOR_UI7Kit_UI7Window 0
+#define COCOAPODS_VERSION_MINOR_UI7Kit_UI7Window 9
+#define COCOAPODS_VERSION_PATCH_UI7Kit_UI7Window 18
+
+// UIActivityIndicator-for-SDWebImage
+#define COCOAPODS_POD_AVAILABLE_UIActivityIndicator_for_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_UIActivityIndicator_for_SDWebImage 1
+#define COCOAPODS_VERSION_MINOR_UIActivityIndicator_for_SDWebImage 0
+#define COCOAPODS_VERSION_PATCH_UIActivityIndicator_for_SDWebImage 5
+
+// UIKitResources/UIAccessoryButton
+#define COCOAPODS_POD_AVAILABLE_UIKitResources_UIAccessoryButton
+#define COCOAPODS_VERSION_MAJOR_UIKitResources_UIAccessoryButton 7
+#define COCOAPODS_VERSION_MINOR_UIKitResources_UIAccessoryButton 0
+#define COCOAPODS_VERSION_PATCH_UIKitResources_UIAccessoryButton 0
+
+// UIKitResources/UIButtonBar
+#define COCOAPODS_POD_AVAILABLE_UIKitResources_UIButtonBar
+#define COCOAPODS_VERSION_MAJOR_UIKitResources_UIButtonBar 7
+#define COCOAPODS_VERSION_MINOR_UIKitResources_UIButtonBar 0
+#define COCOAPODS_VERSION_PATCH_UIKitResources_UIButtonBar 0
+
+// UIKitResources/UITabBar
+#define COCOAPODS_POD_AVAILABLE_UIKitResources_UITabBar
+#define COCOAPODS_VERSION_MAJOR_UIKitResources_UITabBar 7
+#define COCOAPODS_VERSION_MINOR_UIKitResources_UITabBar 0
+#define COCOAPODS_VERSION_PATCH_UIKitResources_UITabBar 0
+
+// ViewDeck
+#define COCOAPODS_POD_AVAILABLE_ViewDeck
+#define COCOAPODS_VERSION_MAJOR_ViewDeck 2
+#define COCOAPODS_VERSION_MINOR_ViewDeck 3
+#define COCOAPODS_VERSION_PATCH_ViewDeck 1
+
+// cdebug
+#define COCOAPODS_POD_AVAILABLE_cdebug
+#define COCOAPODS_VERSION_MAJOR_cdebug 0
+#define COCOAPODS_VERSION_MINOR_cdebug 1
+#define COCOAPODS_VERSION_PATCH_cdebug 1
+
